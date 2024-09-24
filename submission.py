a70c2c09cb8512ffa8cd208f09787fd446a603ba
Thanh Cong Nguyen <tnguyen100@dons.usfca.edu>
2024-09-23 22:37:55 -0700

    Implementing submission.py

---
+++ submission.py
@@ -0,0 +1,11 @@
+import mars_planner
+import routefinder
+
+print("Question 2")
+mars_planner.main()
+print("\n")
+print("Question 3")
+routefinder.main()
+print("\n")
+print("Question 4")
+import marsrovercoloring
